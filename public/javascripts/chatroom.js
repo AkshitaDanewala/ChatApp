c1c4587d41b706f5e35790e0d53a22298757132d
AkshitaDanewala <akshitadanewala@gmail.com>
2023-12-23 16:51:06 +0530

    file update

--- public/javascripts/chatroom.js
+++ public/javascripts/chatroom.js
@@ -1,12 +1,12 @@
 
-var userName = document.querySelector(".userName")
-var userboxname = document.querySelectorAll(".userboxname")
-// var usersbox =  document.querySelector(".usersbox")
-// var incomingMsg = document.querySelector(".incomingMsg")
+// var userName = document.querySelector(".userName")
+// var userboxname = document.querySelectorAll(".userboxname")
+// // var usersbox =  document.querySelector(".usersbox")
+// // var incomingMsg = document.querySelector(".incomingMsg")
 
-userboxname.forEach(function(name){
-    name.addEventListener("click", function(n){
-        userName.innerHTML = n.target.innerHTML
-        // incomingMsg.innerHTML = n.target.innerHTML
-    })
-})
+// userboxname.forEach(function(name){
+//     name.addEventListener("click", function(n){
+//         userName.innerHTML = n.target.innerHTML
+//         // incomingMsg.innerHTML = n.target.innerHTML
+//     })
+// })
